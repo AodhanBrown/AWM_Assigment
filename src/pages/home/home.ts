adeab32cb7b826f6d84d9f5cdd439dad9559c9f4
c15346551 <c15346551@mydit.ie>
2018-10-30 02:58:24 +0000

    android working

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,6 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import leaflet from 'leaflet';
 
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -14,31 +13,29 @@ export class HomePage {
 
   }
 
-  ionViewDidEnter(){
+  ionViewDidEnter() {
     this.loadmap();
   }
 
-  loadmap(){
+  loadmap() {
     this.map = leaflet.map("map").fitWorld();
-    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-      attributions: 'www.tphangout.com',
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
     }).addTo(this.map);
-
     this.map.locate({
       setView: true,
       maxZoom: 10
-    }).on('Finding Location', (e) => {
+    }).on('locationfound', (e) => {
       let markerGroup = leaflet.featureGroup();
-      let marker: any = leaflet.maker([e.latitude, e.longitude]).on('click', () => {
-        alert('Clicked')
+      let marker: any = leaflet.marker([e.latitude, e.longitude]).on('click', () => {
+        alert('Marker clicked');
       })
       markerGroup.addLayer(marker);
       this.map.addLayer(markerGroup);
-    }).on('location error', (err) => {
-      alert(err.message);
+      }).on('locationerror', (err) => {
+        alert(err.message);
     })
-
 
   }
 
