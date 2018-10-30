925684199acecd37b97ee00023d28d47e55ff6f5
c15346551 <c15346551@mydit.ie>
2018-10-30 01:31:20 +0000

    commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import leaflet from 'leaflet';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidEnter(){
+    this.loadmap();
+  }
+
+  loadmap(){
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      attributions: 'www.tphangout.com',
+      maxZoom: 18
+    }).addTo(this.map);
+
+    this.map.locate({
+      setView: true,
+      maxZoom: 10
+    }).on('Finding Location', (e) => {
+      let markerGroup = leaflet.featureGroup();
+      let marker: any = leaflet.maker([e.latitude, e.longitude]).on('click', () => {
+        alert('Clicked')
+      })
+      markerGroup.addLayer(marker);
+      this.map.addLayer(markerGroup);
+    }).on('location error', (err) => {
+      alert(err.message);
+    })
+
 
   }
 
